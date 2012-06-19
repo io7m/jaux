55fe1d6556e8a1043eb5aef663aa16c527100e5a
Mark Raynsford <code@io7m.com>
2012-06-19 19:34:38 +0000

    Update documentation compilation

--- make-xmldoc.sh
+++ make-xmldoc.sh
@@ -37,7 +37,7 @@ saxon \
 
 cp ../ext/structural-0.1.0/*.css . || fatal "could not copy CSS"
 cp ../doc/*.css .                  || fatal "could not copy CSS"
-mv ../javadoc .                    || fatal "coult not insert javadoc"
+cp -r ../target/apidocs/ javadoc   || fatal "coult not insert javadoc"
 
 cd "${CURRENT_DIR}" ||
   fatal "could not restore directory"
